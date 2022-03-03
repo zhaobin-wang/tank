85c314616264f6b3b153fa4c74210e1d662ccd86
wangzhaobin <w185142767523@163.com>
2022-03-03 18:19:51 +0800

    新建工具类

---
+++ java/com.wangzhaobin/java/common/SplitUtil.java
@@ -0,0 +1,12 @@
+package common;
+
+import java.util.List;
+
+/**
+ * @author wangzhaobin
+ * @date 2022/3/3 下午6:16
+ */
+public class SplitUtil {
+
+    //List<String> list = Splitter.on(",").splitToList(Str);
+}
