00d342f73655a8cf4d22f4f988dbd30e849d4d58
wangzhaobin <w185142767523@163.com>
2022-03-05 21:30:28 +0800

    新建GameObject类

---
+++ src/main/java/tank/GameObject.java
@@ -0,0 +1,15 @@
+package tank;
+
+import java.awt.*;
+
+/**
+ * @author wangzhaobin
+ * @date 2022/3/5 下午9:20
+ */
+public abstract class GameObject {
+
+    int x, y;
+
+    public abstract void paint(Graphics g);
+
+}
